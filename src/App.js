ea6b67e82403eb370ede5c885086be5c7f164e71
tamoghna_mobotics <tamoghna@mobotics.in>
2022-12-22 17:54:45 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-// import logo from './logo.svg';
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 function App() {
   return (
     <>
       <Navbar title="textUtils" about="aboutUs" />
+      
       <div className="container my-3">
         <TextForm heading ="Enter your text"/>
+        <About/>
       </div>
+      
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,27 +1,53 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const handleUpClick = ()=>{
-    let newText= text.toLocaleUpperCase();
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
     setText(newText);
-  }
-  const handleChangeing=(event)=>{
-    setText(event.target.value);  
-  }
-  const[text,setText] = useState("Enter text here ");
+  };
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    let newText = " ";
+    setText(newText);
+  };
+  
+  const handleChangeing = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here ");
   return (
     <>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          value={text}
-          onChange={handleChangeing}
-          rows="8"
-        ></textarea>
+      <div className="container my-3">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            value={text}
+            onChange={handleChangeing}
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick={handleUpClick}>convert to uppercase</button>
+        <button className="btn btn-warning mx-3" onClick={handleLowClick}>convert to toLowerCase</button>
+        <button className="btn btn-success " onClick={handleClearClick}>Clear Text</button>
       </div>
-      <button className="btn btn-primary"onClick={handleUpClick}>convert to uppercase</button>
+
+      <div className="container my-3">
+        <h1>
+          <u>TEXT SUMMARY</u>
+        </h1>
+        <p>
+          {text.split(" ").length}words and {text.length}charecters present here
+        </p>
+        <h2>
+          <u>PREVIEW TEXT</u>
+        </h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
